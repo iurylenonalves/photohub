95bff25d179aa165dbc944c9bcd3fcf5f577c341
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-14 00:39:20 +0000

    feat(About): import useTranslations

--- src/client/_components/About.tsx
+++ src/client/_components/About.tsx
@@ -1,11 +1,16 @@
+'use client'
+
 import Image from "next/image";
 import { MessageCircle } from "lucide-react";
+import { useTranslations } from "@/context/TranslationContext";
 
 const About = () => {
+  const { translations } = useTranslations()
+
   return (
     <section className="py-16 px-6 bg-gray-50 scroll-mt-16" id="about">
       <div className="max-w-6xl mx-auto text-center space-y-8">
-        <h2 className="text-4xl font-bold text-gray-900">Bruna Alves</h2>
+        <h2 className="text-4xl font-bold text-gray-900">{translations.aboutTitle}</h2>
         <div className="flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-12">
           <div className="w-full md:w-1/2">
             <Image
@@ -20,27 +25,20 @@ const About = () => {
           </div>
           <div className="w-full md:w-1/2 text-left">
             <p className="text-lg text-gray-700 leading-relaxed text-justify">
-              Oi! Eu sou a Bruna, mas você também pode me chamar de Bru (coisa de paulistana, né?!).
-              Londres sempre foi o meu sonho de adolescente, e em 2018 tive a felicidade de realizá-lo. 
-              Me mudei para cá com meu marido e minha filha, e foi aqui que nossa família cresceu ainda mais, 
-              com a chegada de mais dois filhos.
+              {translations.aboutText1}
             </p>
             <p className="text-lg text-gray-700 mt-4 leading-relaxed text-justify">
-              Desde então, estive em busca de novas experiências, e foi nessa cidade incrível que encontrei o 
-              cenário perfeito para transformar minha paixão em profissão. A fotografia sempre foi meu grande amor, 
-              e meu maior desejo é eternizar momentos especiais —assim como minha mãe sempre fez — porque acredito que 
-              todos merecem guardar lembranças inesquecíveis!
+              {translations.aboutText2}
             </p>
             <div className="mt-8 text-center">
                 <a
-                  href={`https://wa.me/447542554870?text=Olá%2C%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços`}
+                  href={`https://wa.me/447542554870?text=${encodeURIComponent(translations.whatsappMessage)}`}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center gap-2 px-6 py-3 text-white bg-green-600 text-lg font-semibold rounded-md shadow-md hover:bg-green-700 transition"
-                  //className="bg-green-500 text-white px-6 py-3 rounded-lg flex items-center gap-2 hover:bg-green-600 transition"
                 >
                   <MessageCircle size={20} />
-                  Fale comigo no WhatsApp
+                  {translations.whatsappButton}
                 </a>
               </div>
           </div>
