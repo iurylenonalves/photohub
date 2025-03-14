53e7d8bb711b01a831e795b467553ee6c39e214a
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-14 00:43:27 +0000

    feat(Contact): import useTranslations

--- src/client/_components/Contact.tsx
+++ src/client/_components/Contact.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import Image from 'next/image';
 import { MessageCircle } from 'lucide-react';
+import { useTranslations } from '@/context/TranslationContext';
 
 const Contact = () => {
+  const { translations } = useTranslations()
+
   return (
       <section className="py-16 px-6 bg-gray-50 scrool-mt-16" id="contact">
         <div className="max-w-6xl mx-auto text-center space-y-8">
-          <h2 className="text-4xl font-bold text-gray-900">Bruna Alves</h2>
+          <h2 className="text-4xl font-bold text-gray-900">{translations.contactTitle}</h2>
           <div className="flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-12">
             <div className="w-full md:w-1/2">
               <Image
@@ -20,26 +25,26 @@ const Contact = () => {
             </div>
             <div className="w-full md:w-1/2 text-left">
               <h2 className="text-3xl font-bold text-gray-900 mb-6 text-center md:text-center">
-                Entre em Contato
+                {translations.contactSubtitle}
               </h2>              
               <p className="text-lg text-gray-700 mb-4 text-justify">
-                Quer transformar seus momentos em registros inesquecíveis? Seja para sua viagem, sua marca ou um retrato especial, estou aqui para capturar sua essência em cada clique.
+                {translations.contactText1}
               </p>
               <p className="text-lg text-gray-700 mb-4 text-justify">
-                📩 Me mande uma mensagem e vamos conversar sobre o ensaio perfeito para você!
+                📩 {translations.contactText2}
               </p>
               <p className="text-lg text-gray-700 mb-4 text-justify">
-                📸 Vamos eternizar seus momentos juntos?
+                📸 {translations.contactText3}
               </p>
               <div className="mt-8 text-center">
                 <a
-                  href={`https://wa.me/447542554870?text=Olá%2C%20gostaria%20de%20saber%20mais%20sobre%20seus%20serviços`}
+                  href={`https://wa.me/447542554870?text=${encodeURIComponent(translations.whatsappMessage)}`}
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center gap-2 px-6 py-3 text-white bg-green-600 text-lg font-semibold rounded-md shadow-md hover:bg-green-700 transition"
                 >
                   <MessageCircle size={20} />
-                  Fale comigo no WhatsApp
+                  {translations.whatsappButton}
                 </a>
               </div>
             </div>
