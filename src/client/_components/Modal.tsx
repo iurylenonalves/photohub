33ab152286383c7817d85b3158aab1d990ddae29
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-16 02:44:04 +0000

    feat(Modal): add keyboard navigation and improve accessibility with aria-labels

--- src/client/_components/Modal.tsx
+++ src/client/_components/Modal.tsx
@@ -3,21 +3,27 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "../../styles/modal.module.css";
+import { useTranslations } from "@/context/TranslationContext";
 
+// Modal props
 interface ModalProps {
   images: string[];
   selectedIndex: number;
   onClose: () => void;
 }
 
+// Modal component
 const Modal = ({ images, selectedIndex, onClose }: ModalProps) => {
   const [currentIndex, setCurrentIndex] = useState(selectedIndex);
 
-  // Atualiza a imagem quando uma nova modal é aberta
+  const { translations } = useTranslations();
+
+  // Update the current index when the selected index changes
   useEffect(() => {
     setCurrentIndex(selectedIndex);
   }, [selectedIndex]);
 
+  // Handle the previous and next button
   const handlePrev = () => {
     setCurrentIndex((prev) => (prev > 0 ? prev - 1 : images.length - 1));
   };
@@ -26,13 +32,38 @@ const Modal = ({ images, selectedIndex, onClose }: ModalProps) => {
     setCurrentIndex((prev) => (prev < images.length - 1 ? prev + 1 : 0));
   };
 
+  // Navigate with the arrow keys
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "ArrowLeft") {
+        handlePrev();
+      } else if (event.key === "ArrowRight") {
+        handleNext();
+      } else if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleNext, handlePrev, onClose]);
+
   return (
     <div className={styles.modalWrapper}>
-      <button className={styles.closeButton} onClick={onClose}>
+      <button className={styles.closeButton} 
+        onClick={onClose} 
+        aria-label={translations.modalArialLabelClose}
+      >
         ✖
       </button>
 
-      <button className={`${styles.navButton} ${styles.navButtonLeft}`} onClick={handlePrev}>
+      <button className={`${styles.navButton} ${styles.navButtonLeft}`} 
+        onClick={handlePrev}
+        aria-label={translations.modalArialLabelPrevious}
+      >
         ◀
       </button>
 
@@ -40,15 +71,18 @@ const Modal = ({ images, selectedIndex, onClose }: ModalProps) => {
         <Image
           src={images[currentIndex]}
           alt={`Imagem ${currentIndex + 1}`}
-          layout="intrinsic"  // Usando layout "intrinsic" para manter a proporção natural
-          width={1200}  // Largura padrão
-          height={800}  // Altura padrão
-          objectFit="contain"  // Usando contain para não distorcer a imagem
+          layout="intrinsic"  
+          width={1200}
+          height={800}
+          objectFit="contain"
           className="rounded-lg"
         />
       </div>
 
-      <button className={`${styles.navButton} ${styles.navButtonRight}`} onClick={handleNext}>
+      <button className={`${styles.navButton} ${styles.navButtonRight}`} 
+        onClick={handleNext}
+        aria-label={translations.modalArialLabelNext}
+      >
         ▶
       </button>
     </div>
