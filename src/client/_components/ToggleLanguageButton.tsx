0c4c46cc06c89e631947f900d994d2442c2c9473
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-17 23:25:52 +0000

    perf(ToggleLanguageButton): add preload images to improve performance

--- src/client/_components/ToggleLanguageButton.tsx
+++ src/client/_components/ToggleLanguageButton.tsx
@@ -3,12 +3,27 @@
 import { useTranslations } from "@/context/TranslationContext";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import styles from "../../styles/toggleLanguageButton.module.css";
 
 const ToggleLanguageButton = () => {
   const { locale, setLocale } = useTranslations();
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Preload images to improve performance
+    if (typeof window !== 'undefined') {
+      const preloadImages = () => {
+        const enImg = document.createElement('img');
+        enImg.src = '/images/en.svg';
+        
+        const ptImg = document.createElement('img');
+        ptImg.src = '/images/pt.svg';
+      };
+      
+      preloadImages();
+    }
+
+    // Check if the device is mobile based on window width
     const checkIfMobile = () => {
       setIsMobile(window.innerWidth <= 768);
     }
@@ -16,45 +31,36 @@ const ToggleLanguageButton = () => {
     checkIfMobile()
     window.addEventListener('resize', checkIfMobile)
 
+    // Cleanup event listener on component unmount
     return () => {
       window.removeEventListener('resize', checkIfMobile)
     }
   }, [])
 
+  // Toggle the language between English and Portuguese
   const toggleLanguage = () => {
     setLocale(locale === "en" ? "pt" : "en");
   };
-
+  
+  // Determine which flag to show based on the current locale
   const flagToShow = locale === "en" ? "pt" : "en";
   const flagAlt = locale === "en" ? "Brazilian Flag" : "English Flag";
-  const languageText = locale === "en" ? "Português" : "English";
 
   return (
-    <div className="flag-container">
+    <div className={styles.flagContainer}>
       <button 
-      onClick={toggleLanguage} 
-      aria-label={`Switch language to ${languageText}`}
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: isMobile ? "4px" : "8px",
-        border: "none",
-        background: "none",
-        cursor: "pointer",
-        fontSize: isMobile ? "0.8rem" : "1rem",
-      }}
-    >
-      <Image 
-        className="flag-icon"
-        src={`/images/${flagToShow}.svg`} 
-        alt={flagAlt} 
-        width={isMobile ? 36 : 56} 
-        height={isMobile ? 26 : 40}
-        priority={true}
-        style={{ objectFit: "contain" }}
-      />
-      {!isMobile && languageText}
-    </button>
+        onClick={toggleLanguage} 
+        aria-label={`Switch language to ${flagAlt}`}
+        className={styles.toggleButton}
+      >
+        <Image 
+          className={styles.flagIcon}
+          src={`/images/${flagToShow}.svg`} 
+          alt={flagAlt} 
+          width={isMobile ? 36 : 64} 
+          height={isMobile ? 26 : 48}               
+        />     
+      </button>
     </div>
   );
 };
