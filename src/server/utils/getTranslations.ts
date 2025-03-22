bb88d8f9db8d40b41cc6b5a07d0140188331c048
Iury Lenon Alves <iuryalves.uk@gmail.com>
2025-03-22 15:11:22 +0000

    Deleted src/server/utils/getTranslations.ts

--- src/server/utils/getTranslations.ts
+++
@@ -1,5 +0,0 @@
-import { translations, Language, TranslationKeys } from "../utils/translations";
-
-export const getTranslation = (lang: Language): Record<TranslationKeys, string> => {
-  return translations[lang] || translations["en"];
-};
